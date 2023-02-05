a5307eb1ab849ba258fc8c4378cacb80cb1acbc0
Дмитрий Спиряев <imwisagist@yandex.ru>
2023-02-05 11:47:37 +0300

    С новыми силами в бой

---
+++ backend/recipes/admin.py
@@ -0,0 +1,64 @@
+from django.contrib import admin
+
+from .models import (Favorite, Ingredient, IngredientRecipe, Recipe,
+                     ShoppingCart, Tag)
+
+
+class IngredientInline(admin.TabularInline):
+    model = IngredientRecipe
+    extra = 3
+    min_num = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('author', 'name', 'cooking_time',
+                    'get_favorites', 'get_ingredients',)
+    search_fields = ('name', 'author', 'tags')
+    list_filter = ('author', 'name', 'tags')
+    inlines = (IngredientInline,)
+    empty_value_display = 'Пусто'
+
+    def get_favorites(self, obj):
+        return obj.favorites.count()
+    get_favorites.short_description = 'Избранное'
+
+    def get_ingredients(self, obj):
+        return ', '.join([
+            ingredients.name for ingredients
+            in obj.ingredients.all()])
+    get_ingredients.short_description = 'Ингридиенты'
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name', )
+    list_filter = ('name', )
+    empty_value_display = 'Пусто'
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'slug')
+    search_fields = ('name', 'slug')
+    list_filter = ('name', )
+    empty_value_display = 'Пусто'
+
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe')
+    list_filter = ('user', 'recipe')
+    search_fields = ('user', 'recipe')
+    empty_value_display = 'Пусто'
+
+
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'user')
+    list_filter = ('recipe', 'user')
+    search_fields = ('user', )
+    empty_value_display = 'Пусто'
+
+
+admin.site.register(ShoppingCart, ShoppingCartAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Favorite, FavoriteAdmin)
---
+++ backend/users/models.py
@@ -0,0 +1,66 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.db import models
+from django.db.models import F, Q, UniqueConstraint
+
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username', 'first_name', 'last_name', )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=settings.LENGTH_USER
+    )
+    last_name = models.CharField(
+        max_length=settings.LENGTH_USER,
+        verbose_name='Фамилия',
+    )
+    email = models.EmailField(
+        max_length=settings.LENGTH_EMAIL_FIELD_USER,
+        verbose_name='Электронная почта',
+        unique=True
+    )
+    username = models.CharField(
+        verbose_name='Псевдоним',
+        max_length=settings.LENGTH_USER,
+        unique=True,
+        validators=(UnicodeUsernameValidator(), )
+    )
+    class Meta:
+        ordering = ('username', )
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+    def __str__(self):
+        return self.username
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+        related_name='follower',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Подписчик',
+        related_name='following'
+    )
+    class Meta:
+        ordering = ('-id', )
+        constraints = [
+            UniqueConstraint(
+                fields=('user', 'author'),
+                name='unique_follow'
+            ),
+            models.CheckConstraint(
+                check=~Q(user=F('author')),
+                name='no_self_follow'
+            )
+        ]
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+    def __str__(self) -> str:
+        return f"{self.user} подписан на {self.author}"
